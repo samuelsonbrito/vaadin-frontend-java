8804e67de1066df484a7400ccfd1cea143d8c0d6
start.vaadin.com <no-reply@vaadin.com>
2024-12-19 01:51:53 +0000

    Generated project

---
+++ src/main/java/br/com/descompila/views/cadastrodepessoas/CadastrodePessoasView.java
@@ -0,0 +1,76 @@
+package br.com.descompila.views.cadastrodepessoas;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Cadastro de Pessoas")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.USER)
+public class CadastrodePessoasView extends Composite<VerticalLayout> {
+
+    public CadastrodePessoasView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H3 h3 = new H3();
+        FormLayout formLayout2Col = new FormLayout();
+        TextField textField = new TextField();
+        TextField textField2 = new TextField();
+        DatePicker datePicker = new DatePicker();
+        TextField textField3 = new TextField();
+        EmailField emailField = new EmailField();
+        TextField textField4 = new TextField();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Button buttonSecondary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.CENTER);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.setMaxWidth("800px");
+        layoutColumn2.setHeight("min-content");
+        h3.setText("Cadastro de Pessoas");
+        h3.setWidth("100%");
+        formLayout2Col.setWidth("100%");
+        textField.setLabel("Nome");
+        textField2.setLabel("Sobrenome");
+        datePicker.setLabel("Data de Nascimento");
+        textField3.setLabel("Telefone");
+        emailField.setLabel("Email");
+        textField4.setLabel("Profiss'ao");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Salvar");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonSecondary.setText("Cancelar");
+        buttonSecondary.setWidth("min-content");
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(h3);
+        layoutColumn2.add(formLayout2Col);
+        formLayout2Col.add(textField);
+        formLayout2Col.add(textField2);
+        formLayout2Col.add(datePicker);
+        formLayout2Col.add(textField3);
+        formLayout2Col.add(emailField);
+        formLayout2Col.add(textField4);
+        layoutColumn2.add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        layoutRow.add(buttonSecondary);
+    }
+}
